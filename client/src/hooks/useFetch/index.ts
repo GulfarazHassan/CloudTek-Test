04e1eb8235873eb370f02f015c8bfbf834f15ca7
2@Decentfaraz <gulfarazhassan535@gmail.com>
2022-02-05 02:05:25 +0500

    CloudTek

--- client/src/hooks/useFetch/index.ts
+++ client/src/hooks/useFetch/index.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { API_HOST } from './constants';
 import { ItemType } from '../../types/item.type';
